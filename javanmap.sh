3c1fd812c140c01bbccbae4c0805d4b38dbec6cb
Nerix <nerix.main@gmail.com>
2026-06-25 12:45:00 +0200

    Added '-h' and '--help' for help msg; new cycle 'for' for finding IP addresses in cli promt

--- javanmap.sh
+++ javanmap.sh
@@ -1,29 +1,36 @@
 #!/bin/bash
 
 version="v0.2.4"
-ip="${@: -1}"
+for arg in "$@"; do
+    if [[ -z $1 || $arg == "--help" || $arg == "-h" ]]; then
 
-if [[ -z $1 ]]; then
-
-    echo "
-    BEFORE USAGE MAKE SURE JAVA IS INSTALLED!
+        echo "
+        BEFORE USAGE MAKE SURE JAVA IS INSTALLED!
     
-    Javanmap $version ( https://github.com/NerixGO/javanmap )
-    Javanmap is a Nmap wroted from 0 with java language!
-    Usage: javanmap {target specification}
-    
-    TARGET SPECIFICATION:
-        Can pass IP addresses
-        Ex: 192.168.1.1
+        Javanmap $version ( https://github.com/NerixGO/javanmap )
+        Javanmap is a Nmap wroted from scratch with java language!
+        Usage: javanmap {target specification}
+        
+        TARGET SPECIFICATION:
+            Can pass IP addresses
+            Ex: 192.168.0.1
 
-    EXAMPLES:
-        javanmap 192.168.1.1"
+        EXAMPLES:
+            javanmap 192.168.0.1"
 
-    exit
-fi
+        exit 0
+    fi
+done
 
 mkdir -p bin
 if [[ -z "$(find bin -type f -name '*.class' 2>/dev/null)" ]]; then
     javac -d bin *.java
 fi
+
+for arg in "$@"; do
+    if [[ "$arg" =~ ^([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)$ ]]; then 
+        ip="$arg"
+    fi
+done
+
 java -cp bin Main $version $ip
