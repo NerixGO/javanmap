ff1f0c8521a0b030e5c56fbc9bd80b95397a79c8
Nerix <nerix.main@gmail.com>
2026-06-26 20:45:54 +0200

    Fixed problem with Top1000Ports.txt

--- PortCheckTop1000.java
+++ PortCheckTop1000.java
@@ -2,24 +2,26 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class PortCheckTop1000 {
 
-    public void top1000(String ip) {
+    public void top1000(String ip, String dataDir) {
+
         System.out.println("\nPORT");
+        Path portsFile = Paths.get(dataDir, "Top1000Ports.txt");
 
         try {
-            List<Integer> ports = Files.readAllLines(Paths.get(System.getProperty("user.dir"), "Top1000Ports.txt")
-                )
-                .stream()
-                .map(String::trim)
-                .filter(s -> !s.isEmpty())
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
-
+            List<Integer> ports = Files.readAllLines(portsFile)
+                    .stream()
+                    .map(String::trim)
+                    .filter(s -> !s.isEmpty())
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+            
             for (Integer port : ports) {
                 try (Socket socket = new Socket()) {
                     socket.connect(new InetSocketAddress(ip, port), 200);
