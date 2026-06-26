749f5403d53f62d41312307b0691998bf92d3272
Nerix <nerix.main@gmail.com>
2026-06-26 11:57:34 +0200

    Added port support (in --help flag), also made text a little bit more beatiful

--- javanmap.sh
+++ javanmap.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
-version="v0.2.4"
+version="v0.3.1"
 
 for arg in "$@"; do
-    if [[ -z $1 || $arg == "--help" || $arg == "-h" ]]; then
+    if [[ $arg == "--help" || $arg == "-h" ]]; then
 
-        echo "
-    BEFORE USAGE MAKE SURE JAVA IS INSTALLED!
+        printf "\n\033[1m   ---BEFORE USAGE MAKE SURE JAVA IS INSTALLED!---\033[0m\n"
 
+    echo "
     Javanmap $version ( https://github.com/NerixGO/javanmap )
     Javanmap is a Nmap wroted from scratch with java language!
-    Usage: javanmap {target specification}
+    Usage: javanmap [option] <target>
     
     TARGET SPECIFICATION:
         Can pass IP addresses
         Ex: 192.168.0.1
 
     EXAMPLES:
-        javanmap 192.168.0.1"
+        javanmap 192.168.0.1
+        
+    SCAN OPTIONS:
+        -p <port>, --port <port>                Use specific port. If port not specified, scan 1000 Most common used ports"
 
         exit 0
     fi
