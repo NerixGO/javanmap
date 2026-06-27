391c2fe93b32ac09c049d130c63b8686636ba920
Nerix <nerix.main@gmail.com>
2026-06-27 16:54:10 +0200

    extract summary logic and improve program flow

--- Main.java
+++ Main.java
@@ -2,15 +2,16 @@ public class Main {
     public static void main (String[] args) {
 
         ScanOptions opt = ScanOptions.checkArgs(args);
-        PingCheck check = new PingCheck(opt.ip);
-        PortCheckTop1000 ports = new PortCheckTop1000();
-
-        Scan scan = new Scan();
-
+        
         if (opt.ip == null) {
             System.out.println("Error: no target IP provided");
             return;
         }
+
+        PingCheck check = new PingCheck(opt.ip);
+        PortCheckTop1000 ports = new PortCheckTop1000();
+        Scan scan = new Scan();
+
         
         long startTime = System.currentTimeMillis();
         
@@ -20,7 +21,7 @@ public class Main {
         if (!alive) {
             long endTime = System.currentTimeMillis();
             int updown = check.getUpdown();
-            System.out.print("\nJavanmap done: " + opt.QuantityIps + " IP address (" + updown + " hosts up) scanned in " + (double)(endTime - startTime)/1000 + " seconds.\n");
+            System.out.print("\nJavanmap done: " + opt.QuantityIps + " IP address (" + updown + " hosts up) scanned in " + (double)(endTime - startTime)/1000.0 + " seconds.\n");
             System.exit(1);
         }
         
@@ -33,6 +34,6 @@ public class Main {
         long endTime = System.currentTimeMillis();
 
         int updown = check.getUpdown();
-        System.out.print("\nJavanmap done: " + opt.QuantityIps + " IP address (" + updown + " hosts up) scanned in " + (double)(endTime - startTime)/1000 + " seconds.\n");
+        System.out.print("\nJavanmap done: " + opt.QuantityIps + " IP address (" + updown + " hosts up) scanned in " + (double)(endTime - startTime)/1000.0 + " seconds.\n");
     }
 }
