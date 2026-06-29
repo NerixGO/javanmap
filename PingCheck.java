98ab3d6f4d782f49c384e76c420fa4edfaa2e669
Nerix <nerix.main@gmail.com>
2026-06-29 19:29:07 +0200

    Finally works, now it can handle domain names (f.e. google.com)

--- PingCheck.java
+++ PingCheck.java
@@ -1,40 +1,33 @@
 import java.io.IOException;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class PingCheck {
-    
-    public String ip;
-    public int updown = 0;
 
-    public PingCheck(String ip) {
-        this.ip = ip;
-    }
+    private int updown = 0;
 
     public int getUpdown() {
         return updown;
     }
+
     public boolean ping(String ip) {
 
         try {
             InetAddress address = InetAddress.getByName(ip);
 
-            boolean reachable = address.isReachable(2000);
-
-            if(reachable) {
+            if (address.isReachable(2000)) {
                 System.out.println("Javanmap scan report for " + address.getHostName() + " (" + address.getHostAddress() +")");
-                System.out.println("Host is up.");
+                System.out.println("Host is up: ");
                 updown++;
                 return true;
             } else {
-                System.out.println("Note: Host seems down.");
                 return false;
             }
-        } catch (UnknownHostException e) {
-            System.out.println("Error: unknown host.");
-            return false;
+
         } catch (IOException e) {
-            System.out.println("Note: Host unreachable. It may be blocking ICMP probes.");
+            System.out.println("Note: Host seems down");
             return false;
         }
     }
